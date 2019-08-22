851fdf7be3a4de7a693c8656d8adbb716c0d0e8c
dom-dom-dom <speak@dominicmliddell@gmail.com>
2019-08-22 17:40:23 -0500

    adds validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,18 @@
 class Order < ApplicationRecord
   belongs_to :screening
   belongs_to :movie
+
+  
+
+  validates :card, numericality: { only_integer: true }
+
+  validates :email, format: { with: /\A\S+@.+\.\S+\z/,
+    message: "must be an email address" }
+
+  validates :name,  
+	  :email, 
+	  :card,
+	  :exp,
+	  presence: true
+
 end
